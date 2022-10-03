e1d5fe2eed826559f2a34cdb1322439afe23f8d9
Dawiderter <dwalczak456@gmail.com>
2022-10-03 04:59:48 +0200

    Dragging nodes (for now in tests)

--- src/Nodes/TestingShape.hpp
+++ src/Nodes/TestingShape.hpp
@@ -7,14 +7,29 @@ class TestingShape : public Node, public MouseSubscriber
 {
     sf::CircleShape shape = sf::CircleShape(40.f);
 
+    bool mouse_pressed = false;
+    engine::Vec2f rel_clicked_spot;
+
     void onDraw(Renderer &renderer) const override { renderer.draw_object_on_layer(shape,0, global_transform.getTransform()); }
 
     void render_to_color_map(ColorMap& color_map) override {
       color_map.draw_object(shape, global_transform.getTransform());
     }
 
-    void handle_press(sf::Event &event) {
+    void handle_press(sf::Event &event) override {
+      mouse_pressed = true;
+      rel_clicked_spot = {getGlobalTranslation().x - event.mouseButton.x, getGlobalTranslation().y - event.mouseButton.y};
       std::cout<< "Test " << event.mouseButton.x << " " << event.mouseButton.y << std::endl;
+    }
+
+    void handle_move(sf::Event &event) override {
+      if(mouse_pressed){
+        setGlobalTranslation(sf::Vector2f(engine::Vec2f{event.mouseMove.x, event.mouseMove.y} + rel_clicked_spot));
+      }
+    }
+
+    void handle_release(sf::Event &event) override {
+      mouse_pressed = false;
     }
 
     public:
